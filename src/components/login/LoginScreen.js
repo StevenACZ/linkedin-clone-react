e7576fc0b00792010066f60fa44c54073156a370
stevenacz <scoailazaa@gmail.com>
2021-02-22 11:55:20 -0500

    loginScreen improved

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,69 @@
 import React from 'react'
+import useForm from '../../customHooks/useForm'
+import logoLinkedin from '../../images/linkedin.svg'
 
 export const LoginScreen = () => {
+
+  const initialState = {
+    email: '',
+    password: ''
+  }
+
+  const [ formValues, handleInputChange, reset ] = useForm( initialState );
+
+  
+  const { email, password } = formValues;
+
   return (
-    <div>
-      <h1>Login Screen</h1>
+    <div className="login-screen">
+      <div className="login__content">
+        <img className="login__logo" src={ logoLinkedin } />
+
+        <form className="login__form">
+          <h2>Sign in</h2>
+          <p>Stay updated on your professional world</p>
+
+          <div className="form__main">
+            <label>
+              <input
+                type="text"
+                name="email"
+                value={ formValues.email }
+                onChange={ handleInputChange }
+              />
+              <span
+                className={ email.length > 0 ? 'active' : '' }
+              >
+                Email or Phone
+              </span>
+            </label>
+
+            <label>
+              <input
+                type="password"
+                name="password"
+                value={ formValues.password }
+                onChange={ handleInputChange }
+              />
+              <span
+                className={ password.length > 0 ? 'active' : '' }
+              >
+                Password
+              </span>
+            </label>
+          </div>
+
+          <div className="form__forgot-password">
+            <a className="link" href='/'>Forgot password?</a>
+          </div>
+
+          <input className="form__btn-submit" type="submit" value="Sign in"/>
+        </form>
+
+        <div className="join-now">
+          <p>New to LinkedIn? <a className="link" href='/'>Join now</a></p>
+        </div>
+      </div>
     </div>
   )
 }
