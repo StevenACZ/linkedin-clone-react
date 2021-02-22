a58649b12421311403d2b8762929e0ccb3a91ecd
stevenacz <scoailazaa@gmail.com>
2021-02-22 16:18:59 -0500

    more routes added

--- src/routers/DashboardRouter.js
+++ src/routers/DashboardRouter.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 import { HomeScreen } from '../components/home/HomeScreen'
+import { JobsScreen } from '../components/jobs/JobsScreen'
+import { MessagingScreen } from '../components/messaging/MessagingScreen'
+import { NetworkScreen } from '../components/network/NetworkScreen'
+import { NotificationsScreen } from '../components/notifications/NotificationsScreen'
+import { Navbar } from '../components/ui/Navbar'
 
 export const DashboardRouter = () => {
   return (
     <>
-      <div>
+      <Navbar />
+
+      <div className="main-content">
         <Switch>
           <Route exact path="/home" component={ HomeScreen } />
+          <Route exact path="/network" component={ NetworkScreen } />
+          <Route exact path="/jobs" component={ JobsScreen } />
+          <Route exact path="/messaging" component={ MessagingScreen } />
+          <Route exact path="/notifications" component={ NotificationsScreen } />
 
           <Redirect to="/home" />
         </Switch>
