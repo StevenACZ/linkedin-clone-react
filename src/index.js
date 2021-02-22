9c639bd14a5121e3f774e04facc4d09c351b102e
stevenacz <scoailazaa@gmail.com>
2021-02-21 19:36:52 -0500

    improved

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { LinkedinApp } from './LinkedinApp';
+import './stylesheets/main.scss';
 
 ReactDOM.render(
   <LinkedinApp />,
